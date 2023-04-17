2854ffcee5f4d99b814c8b07fe1a8f6128af052c
Tanicorn <Tanapat.s@hotmail.com>
2023-04-17 14:56:24 +0100

    Omen Kills Enemy SFX
    Omen will play randomised sounds when killing an enemy

--- RFS_Project/Source/RFS_Project/Public/PlayerCharacter.h
+++ RFS_Project/Source/RFS_Project/Public/PlayerCharacter.h
@@ -101,12 +101,10 @@ protected:
 	void UpdatePlayerMovementState();
 	/* Function for switching player movement states*/
 	void SwitchMovementState(EPlayerMovementState NewState);
-	/* Blueprint-implementable event for defining what happens when player is killed*/
-	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable)
-		void OnKill() override;
-	/* Blueprint-implementable event for defining what happens when player takes damage*/
-	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable)
-		void BPI_TakeDamage();
+
+	UFUNCTION(BlueprintCallable)
+		void OnKill() override { BPI_OnKill(); };
+
 protected:
 	/* Respawn Point for player*/
 	FVector RespawnPoint;
